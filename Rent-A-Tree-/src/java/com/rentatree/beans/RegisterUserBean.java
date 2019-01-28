9ca60b3aba203b7e5a34c7874fe02510af0b9fa1
cubastanley <44968068+cubastanley@users.noreply.github.com>
2019-01-28 15:21:13 +0000

    Updated RegisterUserBean
    Created the bean for registering a user

--- Rent-A-Tree-/src/java/com/rentatree/beans/RegisterUserBean.java
+++ Rent-A-Tree-/src/java/com/rentatree/beans/RegisterUserBean.java
@@ -5,10 +5,91 @@
  */
 package com.rentatree.beans;
 
+import java.io.Serializable;
+import javax.inject.Named;
+
 /**
  *
  * @author cuba
  */
-public class RegisterUserBean {
+
+@Named
+public class RegisterUserBean implements Serializable {
+    
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private String phoneNo;
+
+    public RegisterUserBean(String username, String password, String firstName, String lastName, String address, String email, String phoneNo) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.phoneNo = phoneNo;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+    
+    
     
 }
